4c2f3e750064c1c29cad1912c10ccb98d1a10471
troyismad99 <troyhamilton@gmail.com>
2021-03-01 08:32:27 -0800

    Problem 575 Distribute Candy

---
+++ 0575-DistributeCandies/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+/* Distribute Candies
+
+Alice has n candies, where the ith candy is of type candyType[i].
+Alice noticed that she started to gain weight, so she visited a doctor.
+
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even).
+Alice likes her candies very much, and she wants to eat the maximum number of
+different types of candies while still following the doctor's advice.
+
+Given the integer array candyType of length n,
+return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+
+Example 1:
+Input: candyType = [1,1,2,2,3,3]
+Output: 3
+Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
+
+Example 2:
+Input: candyType = [1,1,2,3]
+Output: 2
+Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types [1,2], [1,3], or [2,3], she still can only eat 2 different types.
+
+Example 3:
+Input: candyType = [6,6,6,6]
+Output: 1
+Explanation: Alice can only eat 4 / 2 = 2 candies. Even though she can eat 2 candies, she only has 1 type.
+
+Constraints:
+n == candyType.length
+2 <= n <= 104
+n is even.
+-105 <= candyType[i] <= 105
+*/
+
+/*
+ * Runtime: 280 ms       Your runtime beats 55.04 % of csharp submissions.
+ * Memory Usage: 46.1 MB
+ */
+
+namespace _0575_DistributeCandies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(DistributeCandies(new int[] { 1, 1, 2, 2, 3, 3 }));
+            Console.WriteLine(DistributeCandies(new int[] { 1, 1, 2, 3 }));
+            Console.WriteLine(DistributeCandies(new int[] { 6, 6, 6, 6 }));
+
+        }
+
+        static int DistributeCandies(int[] candyType)
+        {
+            // The max is half the candies
+            var maxCandies = candyType.Length / 2;
+
+            // The max candy count is limited by the types
+            var candyTypes = candyType.Distinct().Count();
+
+
+            return Math.Min(maxCandies, candyTypes);
+        }
+
+    }
+}
